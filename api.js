c38bd7229435dc27eeb7794a2843209e3219d1f4
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-03-23 12:04:24 +0000

    trying await/async not fully working

--- api.js
+++ api.js
@@ -1,6 +1,18 @@
-function getPicture(){
-    var proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    fetch(proxyUrl + "http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false")
-    .then(response => response.json())
-    .then(response => document.getElementById("randomcat").src=response)
+async function getPicture() {
+    let proxyUrl = await fetch("https://cors-anywhere.herokuapp.com/");
+    let response = await fetch(proxyUrl + "http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false");
+    let user = await response.json();
+
+    let image = document.createElement("randomcat");
+    image.src = URL.createObjectURL(user);
+    document.body.append(image);
+
+    await new Promise((resolve, reject) => setTimeout(3000));
+
+    image.remove();
+
+    return user;
 }
+
+getPicture();
+
